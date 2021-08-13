2e177d06c8a66bfb426ec590e4147c82651322f7
Gabriel Pereira <78962152+GabriellPereiraa@users.noreply.github.com>
2021-08-13 09:19:05 -0300

    creat

---
+++ layout/menu.php
@@ -0,0 +1,64 @@
+<style>
+    .MenuTopico {
+        color: #274F23 !important;
+        font-weight: bold !important;
+    }
+</style>
+
+<div id="app">
+    <div id="sidebar" class="active">
+        <div class="sidebar-wrapper active ps ps--active-y">
+            <div class="m-4">
+                <!-- <a href="index.php">
+                    <img class="img-fluid" src="https://atilasilvalima.a2hosted.com/projects/EGPCE/Cedis/cdn/imagens/egpce.png" alt="Logo EGPCE">
+                </a> -->
+                <img class="img-fluid" src="https://www.egp.ce.gov.br/wp-content/uploads/sites/8/2021/06/logo-egpce-250x170-1.png" alt="Logo EGPCE">
+            </div>
+            <div class="sidebar-menu">
+                
+                <ul class="menu ms-3">
+                    <li class="sidebar-item">
+                        <a href="../Topico01.php" class="MenuTopico sidebar-link">Tópico 01 - Administração Pública e Paradigmas</a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="../Topico02.php" class="MenuTopico sidebar-link">Tópico 02 - O paradigma da Administração Patrimonial</a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="../Topico03.php" class="MenuTopico sidebar-link">Tópico 03 - O paradigma da Administração Burocrática</a>
+                    </li>
+                    <li class="sidebar-item has-sub">
+                        <a href="#" class="MenuTopico sidebar-link">Tópico 04 - O paradigma da Nova Gestão Pública</a>
+                        <ul class="submenu">
+                            <li class="sidebar-item">
+                                <a href="../Topico04.php#SubTopico4.1" class="sidebar-sub">
+                                    <span>4.1 - O Gerencialismo Puro</span>
+                                </a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a href="../Topico04.php#SubTopico4.2" class="sidebar-sub">
+                                    <span>4.2- A Nova Gestão Pública: diversidade, amplitude e evolução</span>
+                                </a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a href="../Topico04.php#SubTopico4.3" class="sidebar-sub">
+                                    <span>4.3 - Orientação para Resultados: foco básico da Nova Gestão Pública</span>
+                                </a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a href="../Topico04.php#SubTopico4.4" class="sidebar-sub">
+                                    <span>4.4 - Análise Crítica da Nova Gestão Pública</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="../Topico05.php" class="MenuTopico sidebar-link">Tópico 05 - O paradigma da Governança Pública</a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="../Referencias.php" class="MenuTopico sidebar-link">Referencias</a>
+                    </li>
+                </ul>
+            </div>
+            <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+        </div>
+    </div>
